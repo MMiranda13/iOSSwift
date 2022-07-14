f5345c5dd1cf370ab14584113a95c20691cce07b
Manuel Miranda <mirmanuel@gmail.com>
2022-07-14 16:24:33 +0100

    UIsliders e labels a funcionar.

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func heightSliderCHanged(_ sender: UISlider) {
+        let height = String(format: "%.2f",sender.value)
+        
+        heightValue.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f",sender.value)
+        
+        weightValue.text = "\(weight)kg"
+        
+    }
+    
+    
+    
 
 }
 
