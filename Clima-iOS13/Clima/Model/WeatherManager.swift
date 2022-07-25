d7e9629817dab9186d947fa05338230077fa21cf
Manuel Miranda <mirmanuel@gmail.com>
2022-07-25 13:39:24 +0100

    Clima app finished, current location added.

--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 protocol WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
@@ -17,6 +18,11 @@ struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=68423f54f0a2e864f697522078eab194&units=metric"
     
     var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
